cf0884de8515039b0299b3b3e2d95b50e7da13d7
AnasMeshal <Fire-Ball-KwT@hotmail.com>
2020-06-29 17:30:05 +0300

    the mangas list is done

--- src/mangas.js
+++ src/mangas.js
@@ -2,25 +2,28 @@ const mangas = [
     {
         id: "1",
         name: "Attack on Titan Manga",
-        price: "10",
+        price: "3.5",
         img: "https://upload.wikimedia.org/wikipedia/en/d/d6/Shingeki_no_Kyojin_manga_volume_1.jpg",
     },
+
     {
         id: "2",
         name: "Kimetsu No Yaiba Manga",
-        price: "8",
+        price: "2.5",
         img: "https://upload.wikimedia.org/wikipedia/en/0/09/Demon_Slayer_-_Kimetsu_no_Yaiba%2C_volume_1.jpg",
     },
+
     {
         id: "3",
         name: "One Piece Manga",
-        price: "8",
+        price: "3",
         img: "https://upload.wikimedia.org/wikipedia/en/a/a3/One_Piece%2C_Volume_1.jpg",
     },
+
     {
         id:"4",
         name: "Hunter x Hunter",
-        price: "13",
+        price: "2",
         img: "https://upload.wikimedia.org/wikipedia/en/0/0f/Hunter_x_Hunter_cover_-_vol1.jpg",
     },
 ]
