0b5dd60190f97a68af216b788630cecc2e94bafd
AnasMeshal <Fire-Ball-KwT@hotmail.com>
2020-08-16 05:17:46 +0300

    changed the baseurl

--- src/stores/instance.js
+++ src/stores/instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000",
-  // baseURL: "https://gentle-shore-83533.herokuapp.com",
+  // baseURL: "http://localhost:8000",
+  baseURL: "https://gentle-shore-83533.herokuapp.com",
 });
 
 export default instance;
